97e52229c87d54a65dcb7bb36c00a7625412aecd
Ondigo Wendy <wendy.ondigo@student.moringaschool.com>
2021-05-02 20:23:58 +0300

    Section one buttons working.Sum displayed when buttons are clicked.

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-let values=[A , B , C, D];
+function myDate() {
+  var date = document.getElementById("myDate").value;
+  document.getElementById("date").innerHTML = date;
+}
 
-  
+
+
+
+  //Monday
+
+  let values=[A , B , C, D];
 
 function getSumMon() {
 
@@ -18,6 +26,37 @@ sumResult=A+B+C+D;
    
 return document.getElementById("sumResultMon").innerHTML=sumResult;
 }
+
+function getMinPerDay(){
+  let val1=document.getElementById("A").value;
+  let val2=document.getElementById("B").value;
+  let val3=document.getElementById("C").value;
+  let val4=document.getElementById("D").value;
+  
+  A=Number(val1);
+  B=Number(val2);
+  C=Number(val3);
+  D=Number(val4);
+
+  minPerDayResult=Math.min(A, B , C , D);
+
+  return document.getElementById("minPerDayResult").innerHTML = Math.min(A, B , C , D);
+}
+
+function getMaxPerDay(){
+  let val1=document.getElementById("A").value;
+  let val2=document.getElementById("B").value;
+  let val3=document.getElementById("C").value;
+  let val4=document.getElementById("D").value;
+  
+  A=Number(val1);
+  B=Number(val2);
+  C=Number(val3);
+  D=Number(val4);
+  return document.getElementById("maxPerDayResult").innerHTML = Math.max(A, B , C , D);
+}
+
+//Tuesday
 function getSumTue() {
 
   let val1=document.getElementById("A2").value;
@@ -34,6 +73,33 @@ sumResult=A+B+C+D;
    
 return document.getElementById("sumResultTue").innerHTML=sumResult;
 }
+
+function getMinPerDay2(){
+  let val1=document.getElementById("A2").value;
+  let val2=document.getElementById("B2").value;
+  let val3=document.getElementById("C2").value;
+  let val4=document.getElementById("D2").value;
+  
+  A=Number(val1);
+  B=Number(val2);
+  C=Number(val3);
+  D=Number(val4);
+  return document.getElementById("minPerDayResult2").innerHTML = Math.min(A, B , C , D);
+}
+
+function getMaxPerDay2(){
+  let val1=document.getElementById("A2").value;
+  let val2=document.getElementById("B2").value;
+  let val3=document.getElementById("C2").value;
+  let val4=document.getElementById("D2").value;
+  
+  A=Number(val1);
+  B=Number(val2);
+  C=Number(val3);
+  D=Number(val4);
+  return document.getElementById("maxPerDayResult2").innerHTML = Math.max(A, B , C , D);
+}
+//Wednesday
 function getSumWed() {
 
   let val1=document.getElementById("A3").value;
@@ -51,6 +117,32 @@ sumResult=A+B+C+D;
 return document.getElementById("sumResultWed").innerHTML=sumResult;
 }
 
+function getMinPerDay3(){
+  let val1=document.getElementById("A3").value;
+  let val2=document.getElementById("B3").value;
+  let val3=document.getElementById("C3").value;
+  let val4=document.getElementById("D3").value;
+  
+  A=Number(val1);
+  B=Number(val2);
+  C=Number(val3);
+  D=Number(val4);
+  return document.getElementById("minPerDayResult3").innerHTML = Math.min(A, B , C , D);
+}
+
+function getMaxPerDay3(){
+  let val1=document.getElementById("A3").value;
+  let val2=document.getElementById("B3").value;
+  let val3=document.getElementById("C3").value;
+  let val4=document.getElementById("D3").value;
+  
+  A=Number(val1);
+  B=Number(val2);
+  C=Number(val3);
+  D=Number(val4);
+  return document.getElementById("maxPerDayResult3").innerHTML = Math.max(A, B , C , D);
+}
+//Thursday
 function getSumThur() {
 
   let val1=document.getElementById("A4").value;
@@ -68,6 +160,32 @@ sumResult=A+B+C+D;
 return document.getElementById("sumResultThur").innerHTML=sumResult;
 }
 
+function getMinPerDay4(){
+  let val1=document.getElementById("A4").value;
+  let val2=document.getElementById("B4").value;
+  let val3=document.getElementById("C4").value;
+  let val4=document.getElementById("D4").value;
+  
+  A=Number(val1);
+  B=Number(val2);
+  C=Number(val3);
+  D=Number(val4);
+  return document.getElementById("minPerDayResult4").innerHTML = Math.min(A, B , C , D);
+}
+
+function getMaxPerDay4(){
+  let val1=document.getElementById("A4").value;
+  let val2=document.getElementById("B4").value;
+  let val3=document.getElementById("C4").value;
+  let val4=document.getElementById("D4").value;
+  
+  A=Number(val1);
+  B=Number(val2);
+  C=Number(val3);
+  D=Number(val4);
+  return document.getElementById("maxPerDayResult4").innerHTML = Math.max(A, B , C , D);
+}
+//Friday
 function getSumFri() {
 
   let val1=document.getElementById("A5").value;
@@ -85,6 +203,32 @@ sumResult=A+B+C+D;
 return document.getElementById("sumResultFri").innerHTML=sumResult;
 }
 
+function getMinPerDay(){
+  let val1=document.getElementById("A5").value;
+  let val2=document.getElementById("B5").value;
+  let val3=document.getElementById("C5").value;
+  let val4=document.getElementById("D5").value;
+  
+  A=Number(val1);
+  B=Number(val2);
+  C=Number(val3);
+  D=Number(val4);
+  return document.getElementById("minPerDayResult").innerHTML = Math.min(A, B , C , D);
+}
+
+function getMaxPerDay5(){
+  let val1=document.getElementById("A5").value;
+  let val2=document.getElementById("B5").value;
+  let val3=document.getElementById("C5").value;
+  let val4=document.getElementById("D5").value;
+  
+  A=Number(val1);
+  B=Number(val2);
+  C=Number(val3);
+  D=Number(val4);
+  return document.getElementById("maxPerDayResult").innerHTML = Math.max(A, B , C , D);
+}
+//Saturday
 function getSumSat() {
 
   let val1=document.getElementById("A6").value;
@@ -102,6 +246,32 @@ sumResult=A+B+C+D;
 return document.getElementById("sumResultSat").innerHTML=sumResult;
 }
 
+function getMinPerDay6(){
+  let val1=document.getElementById("A6").value;
+  let val2=document.getElementById("B6").value;
+  let val3=document.getElementById("C6").value;
+  let val4=document.getElementById("D6").value;
+  
+  A=Number(val1);
+  B=Number(val2);
+  C=Number(val3);
+  D=Number(val4);
+  return document.getElementById("minPerDayResult6").innerHTML = Math.min(A, B , C , D);
+}
+
+function getMaxPerDay6(){
+  let val1=document.getElementById("A6").value;
+  let val2=document.getElementById("B6").value;
+  let val3=document.getElementById("C6").value;
+  let val4=document.getElementById("D6").value;
+  
+  A=Number(val1);
+  B=Number(val2);
+  C=Number(val3);
+  D=Number(val4);
+  return document.getElementById("maxPerDayResult6").innerHTML = Math.max(A, B , C , D);
+}
+//Sunday
 function getSumSun() {
 
   let val1=document.getElementById("A7").value;
@@ -119,18 +289,32 @@ sumResult=A+B+C+D;
 return document.getElementById("sumResultSun").innerHTML=sumResult;
 }
 
-
-
-
-
-
-function getMinPerDay(){
-  return document.getElementById("minPerDayResult").innerHTML = Math.min(A, B , C , D);
+function getMinPerDay7(){
+  let val1=document.getElementById("A7").value;
+  let val2=document.getElementById("B7").value;
+  let val3=document.getElementById("C7").value;
+  let val4=document.getElementById("D7").value;
+  
+  A=Number(val1);
+  B=Number(val2);
+  C=Number(val3);
+  D=Number(val4);
+  return document.getElementById("minPerDayResult7").innerHTML = Math.min(A, B , C , D);
+}
+function getMaxPerDay7(){
+  let val1=document.getElementById("A7").value;
+  let val2=document.getElementById("B7").value;
+  let val3=document.getElementById("C7").value;
+  let val4=document.getElementById("D7").value;
+  
+  A=Number(val1);
+  B=Number(val2);
+  C=Number(val3);
+  D=Number(val4);
+  return document.getElementById("maxPerDayResult7").innerHTML = Math.max(A, B , C , D);
 }
 
-function getMaxPerDay(){
-  return document.getElementById("maxPerDayResult").innerHTML = Math.max(A, B , C , D);
-}
+
 
 function getAvgPerDay(){
   
@@ -181,3 +365,5 @@ function getAvgPerYear(){
   avgPerYearResult
 }
 
+
+let 
